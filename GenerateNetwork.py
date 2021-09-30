80dc40c48109332c7ef55bad0288b7fe02d44d3d
IBPAD <analise@ibpad.com.br>
2021-09-30 12:57:17 -0300

    Update GenerateNetwork.py

--- GenerateNetwork.py
+++ GenerateNetwork.py
@@ -18,7 +18,7 @@ for f in glob.glob('twitter-users/*.json'):
     print("loading " + str(f))
     data = json.load(open(f))
     screen_name = data['screen_name']
-    users[screen_name] = { 'followers': data['followers_count'], 'id':data['id'] }
+    users[screen_name] = { 'followers': data['followers_count'], 'id':data['id'], 'name':data['name'] }
 
 def process_follower_list(screen_name, edges=[], depth=0, max_depth=5):
     f = os.path.join('following', screen_name + '.csv')
@@ -47,10 +47,42 @@ def process_follower_list(screen_name, edges=[], depth=0, max_depth=5):
 
 edges = process_follower_list(SEED, max_depth=5)
 
-with open('twitter_network.csv', 'w') as outf:
+def process_follower_nodes(screen_name, nodes=[], depth=0, max_depth=5):
+    f = os.path.join('following', screen_name + '.csv')
+    print("processing " + str(f))
+
+    if not os.path.exists(f):
+        return nodes
+
+    followers = [line.strip().split('\t') for line in open(f)]
+
+    for follower_data in followers:
+        if len(follower_data) < 2:
+            continue
+
+        screen_name_2 = follower_data[1]
+
+        nodes.append([users[screen_name]['id'], users[screen_name]['name']])
+
+        if depth+1 < max_depth:
+            process_follower_nodes(screen_name_2, nodes, depth+1, max_depth)
+
+    return nodes
+
+nodes = process_follower_nodes(SEED, max_depth=5)
+
+with open('twitter_network_edges.csv', 'w') as outf:
     edge_exists = {}
     for edge in edges:
         key = ','.join([str(x) for x in edge])
         if not(key in edge_exists):
             outf.write('%s,%s,%d\n' % (edge[0], edge[1], edge[2]))
             edge_exists[key] = True
+
+with open('twitter_network_nodes.csv', 'w') as outf:
+    node_exists = {}
+    for node in nodes:
+        key = ','.join([str(x) for x in node])
+        if not(key in node_exists):
+            outf.write('%s,%s\n' % (node[0], node[1]))
+            node_exists[key] = True
